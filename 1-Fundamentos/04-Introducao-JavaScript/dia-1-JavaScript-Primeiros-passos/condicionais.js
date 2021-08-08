0cfbe22997b9dbc155cf9050f8071df11e534698
Murilo Costa <mcosta12358@gmail.com>
2021-08-08 20:06:58 -0300

    implementando as questoes 2,3 e 4

--- 1-Fundamentos/04-Introducao-JavaScript/dia-1-JavaScript-Primeiros-passos/condicionais.js
+++ 1-Fundamentos/04-Introducao-JavaScript/dia-1-JavaScript-Primeiros-passos/condicionais.js
@@ -1,3 +1,14 @@
 /* Exercício da parte do Condições If e Else */
 
-const notaPessoaCandidata = 50;
+const notaPessoaCandidata = 80;
+
+if (notaPessoaCandidata >= 80){
+
+    console.log("Parabéns, você foi aprovado(o)!");
+}
+else if (notaPessoaCandidata < 80 && notaPessoaCandidata >= 60){
+    console.log ("Você está na nossa lista de espera");
+}
+else {
+    console.log("Você foi reprovado(o)")
+}
