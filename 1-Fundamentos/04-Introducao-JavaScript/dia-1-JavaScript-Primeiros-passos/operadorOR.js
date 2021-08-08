05a51038fa074f8ac93a213f57f6a6773542779c
Murilo Costa <mcosta12358@gmail.com>
2021-08-08 20:30:03 -0300

    implementando soluçao do exercicio do operador logico OR

---
+++ 1-Fundamentos/04-Introducao-JavaScript/dia-1-JavaScript-Primeiros-passos/operadorOR.js
@@ -0,0 +1,12 @@
+/* Resolução do exercicio referente ao operador OR */
+
+let weekDay = "quarta-feira";
+
+if ( weekDay === "segunda-feira" ||  weekDay === "terca-feira" ||  weekDay === "quarta-feira" ||  weekDay === "quinta-feira" ||  weekDay === "sexta-feira")
+    {
+        console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+    }
+else {
+    console.log ("FINALMENTE, descando merecido UwU");
+}
+
