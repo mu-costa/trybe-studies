e0808f786a21c81e16c2f4634d45de36a8118163
Murilo Costa <mcosta12358@gmail.com>
2021-08-09 13:51:37 -0300

    Resoluçao da questao 4

--- 1-Fundamentos/04-Introducao-JavaScript/dia-1-JavaScript-Primeiros-passos/resolucao.js
+++ 1-Fundamentos/04-Introducao-JavaScript/dia-1-JavaScript-Primeiros-passos/resolucao.js
@@ -1,14 +1,14 @@
-const valor1 = 4;
-const valor2 = 5;
-const valor3 = 7;
+const valor = -3;
 
-if ( valor1 > valor2 && valor1 > valor3){
+if (valor > 0 ){
 
-    console.log("Valor 1 é o maior");
-} else if (valor2 > valor1 && valor2 > valor3 ) {
+    console.log("Valor positivo");
+}
+else if ( valor === 0){
 
-    console.log("Valor 2 é o maior");
-} else if ( valor3 > valor1 && valor3 > valor2){
-    
-    console.log ("Valor 3 é o maior");
+    console.log ("Valor igual a zero");
+}
+else {
+
+    console.log ("Valor negativo");
 }
