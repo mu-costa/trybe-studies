6181e21c3baa5cec6280e9ae1bca507a75699a21
Murilo Costa <mcosta12358@gmail.com>
2021-09-08 22:36:10 -0300

    commit bomba

---
+++ 1-Fundamentos/04-Introducao-JavaScript/dia-1-JavaScript-Primeiros-passos/switchCase.js
@@ -0,0 +1,19 @@
+let estadoPessoaCandidata = "aprovada";
+
+switch (estadoPessoaCandidata){
+    
+    case "aprovada":
+    console.log("Pessoa aprovada");
+    break;
+    
+    case "reprovada":
+        console.log("Pessoa reprovada");
+
+    break;
+    
+    case "lista":
+        console.log("Pessoa listada");
+    
+    default:
+        console.log("Não se aplica");
+}
