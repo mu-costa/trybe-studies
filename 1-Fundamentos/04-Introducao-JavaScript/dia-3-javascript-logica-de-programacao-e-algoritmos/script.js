685d07be913602616bc569d877bbc78837d8aaff
Murilo Costa <mcosta12358@gmail.com>
2021-10-16 17:00:55 -0300

    resolvendo a questao 2

--- 1-Fundamentos/04-Introducao-JavaScript/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ 1-Fundamentos/04-Introducao-JavaScript/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -1,8 +1,25 @@
-function q1 (){
-    let asteriscos = '*****';
-    for (let index = 0; index < 5; index++) {
-        
-        console.log(asteriscos);
-    }
+function q1() {
+  let asteriscos = "*****";
+  for (let index = 0; index < 5; index++) {
+    console.log(asteriscos);
+  }
 }
 q1();
+/* 
+n = 5
+*
+**
+***
+****
+***** 
+*/
+function q2() {
+  let asterisco = "*";
+  let linha = '';
+
+  for (let index = 0; index < 5; index++) {
+    linha = linha + asterisco;
+    console.log(linha);
+  }
+}
+q2();
