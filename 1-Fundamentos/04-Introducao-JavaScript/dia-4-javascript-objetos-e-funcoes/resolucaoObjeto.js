4170233c0cdeeb443ed6a1cc0e3091c37a992f32
Murilo Costa <mcosta12358@gmail.com>
2021-10-19 23:53:40 -0300

    Resoluçao da atividade relacionada ao assunto de objetos

---
+++ 1-Fundamentos/04-Introducao-JavaScript/dia-4-javascript-objetos-e-funcoes/resolucaoObjeto.js
@@ -0,0 +1,18 @@
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+
+info["recorrente"] = "Sim";
+
+let info2 = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "Sim",
+}; 
+
+for (const key in info) {
+  console.log(info2[key]);
+}
