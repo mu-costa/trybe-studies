66cb86830bdf0f979769736796dd05eec847efa3
Murilo Costa <mcosta12358@gmail.com>
2021-11-16 22:54:17 -0300

    Respondendo a primeira questao do dia 3

---
+++ 1-Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.js
@@ -0,0 +1,9 @@
+function sum(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+      throw new Error('parameters must be numbers');
+    }
+  
+    return a + b;
+  }
+sum(4,'5');
+module.exports = sum;
---
+++ 1-Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -0,0 +1,14 @@
+const { expect } = require("@jest/globals");
+const sum = require("./sum");
+
+describe('Função soma', () => {
+    it('A soma de 4 e 5 retorna 9', () =>{
+        expect(sum(4,5)).toEqual(9);
+    })
+    it('A soma de 0 e 0 retorna 0', ()=>{
+        expect(sum(0,0)).toEqual(0);
+    })
+    it ('Erro ao se introduzir um valor que não é um number', ()=>{
+        expect(sum(4,'5')).toEqual(9);
+    })
+});
