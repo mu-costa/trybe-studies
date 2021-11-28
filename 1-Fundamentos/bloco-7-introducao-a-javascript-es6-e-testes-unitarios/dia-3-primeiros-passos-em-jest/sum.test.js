413a9fbedac867dde972f94ba5836a058d57422d
Murilo Costa <mcosta12358@gmail.com>
2021-11-28 03:38:51 -0300

    enviando exercicios

--- 1-Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ 1-Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,4 +1,3 @@
-const { expect } = require("@jest/globals");
 const sum = require("./sum");
 
 describe('Função soma', () => {
---
+++ 1-Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/testes/teste.js
@@ -0,0 +1,15 @@
+function myRemove(arr, item) {
+    let newArr = [];
+    for (let index = 0; index < arr.length; index += 1) {
+      if (item !== arr[index]) {
+        newArr.push(arr[index]);
+      }
+    }
+    return newArr;
+  }
+  
+  // implemente seus testes aqui
+
+  test('item removido',()=>{
+      expect(myRemove([1, 2, 3, 4], 3).toEqual([1,2,4]));
+  })  
