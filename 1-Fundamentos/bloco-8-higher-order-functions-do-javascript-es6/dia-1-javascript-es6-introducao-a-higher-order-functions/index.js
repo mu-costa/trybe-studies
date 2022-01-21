dea885662acc976029ae4d470d8d66c9be54bdea
Murilo Costa <mcosta12358@gmail.com>
2022-01-21 15:52:31 -0300

    solucao da questao 1

--- 1-Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/index.js
+++
@@ -1,9 +0,0 @@
-const wakeUp = string => 'Wake up';
-
-const drinkCoffee = string => `Let's have coffee`;
-
-const wentToSleep = string => 'Went to sleep!!';
-
-const doingThings = callback => console.log(callback());
-
-doingThings(drinkCoffee);
