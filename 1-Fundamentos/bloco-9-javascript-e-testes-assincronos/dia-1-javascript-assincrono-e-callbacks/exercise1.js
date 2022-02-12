0fe22bebecb3cddb84b7b3f5cc1c6ec624c1bfb2
Murilo Costa <mcosta12358@gmail.com>
2022-02-12 13:18:16 -0300

    resolvendo a questao 4

--- 1-Fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercise1.js
+++ 1-Fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercise1.js
@@ -5,9 +5,22 @@ const getMarsTemperature = () => {
   return Math.floor(Math.random() * maxTemperature);
 };
 
-// crie a função sendMarsTemperature abaixo
-function sendMarsTemperature () {
-    const temperature = getMarsTemperature();
-    return `Mars temperature is: ${temperature} degree Celsius`;
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
+
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+
+const greet = (temperature) =>
+  console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+// definição da função sendMarsTemperature...
+function sendMarsTemperature (callback){
+    if (callback === temperatureInFahrenheit){
+        return callback(getMarsTemperature());
+    }
+    if (callback === greet){
+        return callback(getMarsTemperature());
+    }
 }
-console.log(sendMarsTemperature()); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
+sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
+sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
