ef266b9777493e1e4eb279a1f224bb206ba40835
Murilo Costa <mcosta12358@gmail.com>
2022-02-16 19:27:01 -0300

    resolvendo a questao 3

---
+++ 2- Front-End/bloco-10-introducao-a-react/dia-1-hello-world-no-react/mu-app2/src/Content.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const conteudos = [
+    {
+      conteudo: 'High Order Functions',
+      bloco: 8,
+      status: 'Aprendido'
+    },
+    {
+      conteudo: 'Composicao de Componentes',
+      bloco: 11,
+      status: 'Aprendendo',
+    },
+    {
+      conteudo: 'Composicao de Estados',
+      bloco: 12,
+      status: 'Aprenderei'
+    },
+    {
+      conteudo: 'Redux',
+      bloco: 16,
+      status: 'Aprenderei'
+    },
+  ];
+  
+class Content extends React.Component {
+    render (key){
+        return (
+            conteudos.map(({conteudo,bloco,status}) => {
+                return <div>
+                        <span>O conteúdo é : {conteudo}</span>
+                        <p>Status: {bloco}</p>
+                        <p>Bloco:  {status}</p>
+                        <hr></hr>                               
+                     </div>
+            })
+        )
+    }
+}
+
+export default Content;
---
+++ 2- Front-End/bloco-10-introducao-a-react/dia-1-hello-world-no-react/mu-app2/src/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const contentFrontEnd = <h1>Conteúdos de Front-End</h1>;
+class Header extends React.Component {
+    render(){
+        return (
+            contentFrontEnd
+        )
+    }
+}
+
+export default Header;
