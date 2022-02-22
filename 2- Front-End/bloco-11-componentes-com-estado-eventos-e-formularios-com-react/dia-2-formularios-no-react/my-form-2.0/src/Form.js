30f09633dd60147fae3036311f0ffb5e37673786
Murilo Costa <mcosta12358@gmail.com>
2022-02-22 19:23:59 -0300

    iniciando a resoluçao da atividade

---
+++ 2- Front-End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/Form.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Form extends React.Component {
+    render (){
+        return(
+            <>
+                <fieldset>
+                    
+                </fieldset>
+            </>
+            )
+    }
+}
+
+export default Form;
