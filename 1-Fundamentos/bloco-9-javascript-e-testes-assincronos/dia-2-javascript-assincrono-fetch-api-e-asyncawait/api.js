af4aca47d538cdd3ab6baae037d892d487c58498
Murilo Costa <mcosta12358@gmail.com>
2022-03-30 19:11:51 -0300

    adicionando a funcionalidade de converter para brl

--- 1-Fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/api.js
+++ 1-Fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/api.js
@@ -1,21 +1,31 @@
-const API_URL = 'https://api.coincap.io/v2/assets';
+const API_URL = "https://api.coincap.io/v2/assets";
+const API_URL_2 =
+  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json";
 
-const fetchCrypto = () =>{
-    const obj = {
-        method: 'GET',
-        headers: {'Accept': 'applications/json'}
-    };
+const fetchCrypto = async () => {
+  let valuetoConvert = 0;
+  await fetch(API_URL_2).then((price) => price.json())
+    .then((data) => valuetoConvert = data.usd.brl);
 
-    fetch(API_URL,obj)
-      .then(response => response.json())
-      .then( coins =>   
-       coins.data.filter((coin, index) => index < 10 ? coin : null).map((coin) => {
-           const p = document.createElement('p');
-           const div = document.getElementById('crypto-coins');
-           p.innerText = `${coin.name} (${coin.symbol}) : ${parseFloat(coin.priceUsd).toFixed(2)}`
-           div.appendChild(p);
-       })
-      )
-}
+  const obj = {
+    method: "GET",
+    headers: { Accept: "applications/json" },
+  };
+
+  fetch(API_URL, obj)
+    .then((response) => response.json())
+    .then((coins) =>
+      coins.data
+        .filter((coin, index) => (index < 10 ? coin : null))
+        .map((coin) => {
+          const p = document.createElement("p");
+          const div = document.getElementById("crypto-coins");
+          p.innerText = `${coin.name} (${coin.symbol}) : ${parseFloat(
+            coin.priceUsd * valuetoConvert
+          ).toFixed(2)}`;
+          div.appendChild(p);
+        })
+    );
+};
 
 window.onload = () => fetchCrypto();
