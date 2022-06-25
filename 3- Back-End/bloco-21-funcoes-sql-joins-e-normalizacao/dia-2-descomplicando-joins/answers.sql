d47b0b9222f834c31577f73d85897d98a3d2fe7c
Murilo Costa <mcosta12358@gmail.com>
2022-06-25 13:59:21 -0300

    resolução dos exercicios do dia

---
+++ 3- Back-End/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/answers.sql
@@ -0,0 +1,34 @@
+/* primeira questão */
+SELECT mo.title, box.domestic_sales, box.international_sales 
+FROM pixar.movies AS mo 
+INNER JOIN pixar.box_office AS box 
+ON mo.id = box.movie_id;
+
+/* segunda questão */
+SELECT mo.title, box.domestic_sales + box.international_sales AS total_vendas   
+FROM pixar.movies AS mo    
+INNER JOIN pixar.box_office AS box    
+ON mo.id = box.movie_id   
+WHERE box.international_sales > box.domestic_sales;
+
+/* terceira questão */
+SELECT mo.title, box.rating
+FROM pixar.movies AS mo
+INNER JOIN pixar.box_office AS box
+ON mo.id = box.movie_id;
+
+/* quarta questão */
+
+SELECT  *
+FROM pixar.theater AS thea
+LEFT JOIN pixar.movies AS mov
+ON thea.id = mov.theater_id
+ORDER BY thea.name;
+
+/* quinta questão */
+
+SELECT  mov.*, thea.* FROM pixar.theater AS thea 
+RIGHT JOIN pixar.movies AS mov 
+ON mov.theater_id = thea.id 
+ORDER BY thea.name;
+
