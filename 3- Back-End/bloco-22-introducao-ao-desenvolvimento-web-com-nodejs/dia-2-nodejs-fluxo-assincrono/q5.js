5d6fb775f23d22506ee77133ba7d64129002543a
Murilo Costa <mcosta12358@gmail.com>
2022-07-05 19:06:00 -0300

    q5

--- 3- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/q5.js
+++ 3- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/q5.js
@@ -15,3 +15,18 @@ Promise.all([
 })
   .catch((err) => console.log(err));
 
+
+  Promise.all([
+      fs.readFile('file1.txt','utf-8'),
+      fs.readFile('file2.txt','utf-8'),
+      fs.readFile('file3.txt','utf-8'),
+      fs.readFile('file4.txt','utf-8'),
+      fs.readFile('file5.txt','utf-8'),
+  ]).then((arr)=>{
+      let string = '';
+      arr.forEach((e) => {
+          string = string + " " + JSON.parse(e);
+          return fs.writeFile('./fileAll.txt', JSON.stringify(string));
+      })
+   })
+   .catch((err)=>console.log(err)); 
