b4239337c7e994c93a0b48ec3d241380f75840b4
Murilo Costa <mcosta12358@gmail.com>
2022-07-07 19:28:46 -0300

    q1

---
+++ 3- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/src/calculaSituacao.js
@@ -0,0 +1,7 @@
+const calculaSituacao = (num) => {
+    if( num > 0) return "positivo";
+    if( num < 0) return "negativo";
+    if( num === 0) return "neutro";
+}
+
+module.exports = calculaSituacao;
---
+++ 3- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/test/test1.test.js
@@ -0,0 +1,28 @@
+const { expect } = require('chai');
+const calculaSituacao = require('../src/calculaSituacao');
+
+describe("Testa a primeira função", () => {
+    it(("Testa se a função retorna positivo caso o número seja maior que zero"), ()=>{
+        // arrange
+        const resposta = calculaSituacao(9);
+        // act
+        // assert 
+       expect(resposta).to.be.equals("positivo"); 
+    });   
+
+    it(("Testa se a função retorna negativo caso o número seja menor que zero"), ()=>{
+        // arrange
+        const resposta = calculaSituacao(-9);
+        // act
+        // assert 
+       expect(resposta).to.be.equals("negativo"); 
+    });  
+
+    it(("Testa se a função retorna neutro caso o número seja igual a zero"), ()=>{
+        // arrange
+        const resposta = calculaSituacao(0);
+        // act
+        // assert 
+       expect(resposta).to.be.equals("neutro"); 
+    });      
+});
