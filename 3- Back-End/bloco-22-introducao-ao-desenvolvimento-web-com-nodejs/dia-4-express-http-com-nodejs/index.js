45815fe225cab9ed725fc49f136a6f5c4e4094e1
Murilo Costa <mcosta12358@gmail.com>
2022-07-09 19:21:18 -0300

    q4

--- 3- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ 3- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -17,6 +17,23 @@ app.post('/hello', (req, res) => {
     res.json({ message: `Hello, ${name}!`});
 });
 
+app.post('/greetings', (req,res) => {
+    const { name, age } = req.body;
+
+    if ( age > 17 ){
+        return res.status(200).json({ message: `Hello, ${name}!`})
+    }else{
+        return res.status(401).json({ message: "Unauthorized"})
+    }
+
+});
+
+app.put("/users/:name/:age",(req,res) => {
+    const { name, age } = req.params;
+
+    res.status(204).json({message: `Seu nome é ${name} e você tem ${age} anos de idade`});
+});
+
 app.listen(3001,() =>{
     console.log("Aplicação rodando");
-})
+});
