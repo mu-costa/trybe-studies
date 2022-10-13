7717a9d8175c20f0c4ab3198f8e152bfbd865e30
Murilo Costa <mcosta12358@gmail.com>
2022-10-12 21:25:57 -0300

    answer 1

---
+++ 3- Back-End/bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/exercise.ts
@@ -0,0 +1,11 @@
+class Student {
+    private _matricula: string;
+    private _nome: string;
+    private _nota1Prova: number;
+    private _nota2Prova: number;
+    private _nota3Prova: number;
+    private _nota4Prova: number;
+    private _nota1Trabalho: number;
+    private _nota2Trabalho: number;
+
+}
